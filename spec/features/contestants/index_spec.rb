e4b978e77cdaa3b2b296536f36f4a0c49c9288ef
Noah van Ekdom <noahvanekdom@gmail.com>
2022-09-06 16:22:29 -0600

    Add contestant index page with projects that they participated in

--- spec/features/contestants/index_spec.rb
+++ spec/features/contestants/index_spec.rb
@@ -1,22 +1,13 @@
-# As a visitor,
-# When I visit the contestants index page ("/contestants")
-# I see a list of names of all the contestants
-# And under each contestants name I see a list of the projects (names) that they've been on
-
-# (e.g.   Kentaro Kameyama
-#         Projects: Litfit, Rug Tuxedo
-
-#         Jay McCarroll
-#         Projects: LeatherFeather)
-
 require 'rails_helper'
 
 RSpec.describe 'Contestant Index Page' do
   before(:each) do
     @furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
+    @recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
 
     @upholstery_tux = @furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
     @lit_fit = @furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
+    @boardfit = @recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
 
     @jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
     @gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
@@ -35,8 +26,12 @@ RSpec.describe 'Contestant Index Page' do
       expect(page).to have_content(@kentaro.name)
     end
 
-    xit 'And under each contestants name I see a list of the projects (names) that they have been on' do
-      expect(@jay.name).to appear_before("Litfit")
+    it 'And under each contestants name I see a list of the projects (names) that they have been on' do
+      visit '/contestants'
+      expect(page).to have_content(@upholstery_tux.name)
+      expect(page).to_not have_content(@boardfit.name)
+
+      expect(@jay.name).to appear_before(@lit_fit.name)
     end
   end
 end
