fe0c6350d360b85e36011df146a0dd3ad7a869fc
Noah van Ekdom <noahvanekdom@gmail.com>
2022-09-06 16:37:13 -0600

    Add contestant_count method for project model

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -16,10 +16,11 @@ RSpec.describe Project, type: :model do
   describe "#contestant_count" do
     it 'returns the number of contestants for a project' do
       furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
-      upholstery_tux = @furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
+      upholstery_tux = furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
       jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
       upholstery_tux.contestants << jay
 
       expect(upholstery_tux.contestant_count).to eq 1
     end
+  end
 end
